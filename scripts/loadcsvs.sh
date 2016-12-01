23cde4c60f712138ebf82a97e3d44bbce9017817
nacho <elwanax@gmail.com>
2016-12-01 19:28:46 +0100

    timestamp added
    'date.time.format'='yyyy-MM-dd-HH.mm.ss.SSSSSS'

--- scripts/loadcsvs.sh
+++ scripts/loadcsvs.sh
@@ -24,8 +24,8 @@ do
   ## Creating the load statement on a ddl file
   echo -e "\n@@@@@@@@@@@@@@@"
   echo "Creating the $f.ddl file for the load on BigSQL..."
-  echo "load hadoop using file url '/files/$csvfile' with source properties ('field.delimiter'='|','ignore.extra.fields'='false', 'date.time.format'='yyyyMMdd') into table ZWH.$f overwrite with load properties ('rejected.records.dir'='/tmp/rejected_records/$f.out','max.rejected.records'=0,'num.map.tasks'=8);" >> ./ddl/$f.ddl
-
+  echo "load hadoop using file url '/files/$csvfile' with source properties ('field.delimiter'='|','ignore.extra.fields'='false', 'date.time.format'='yyyyMMdd','date.time.format'='yyyy-MM-dd-HH.mm.ss.SSSSSS') into table ZWH.$f overwrite with load properties ('rejected.records.dir'='/tmp/rejected_records/$f.out','max.rejected.records'=0,'num.map.tasks'=8);" >> ./ddl/$f.ddl
+  
   ## Loading the data with JQSH
   echo "Loading the data of the $f table on BigSQL with JSQSH."
   /usr/ibmpacks/common-utils/current/jsqsh/bin/jsqsh bigsql -i ./ddl/$f.ddl 2> ./out/jsqsh-$f.out
